7a11faf00e58030d582c4d8ef05da4840c12b462
Vinor <vinorval@yandex.ru>
2024-09-03 11:53:27 +0300

    add the second version on the navigation layout

--- src/components/app/navigation/Navigation.js
+++ src/components/app/navigation/Navigation.js
@@ -1,19 +1,19 @@
 import React from "react";
 import Styles from "./Navigation.module.css";
-import { Link } from "react-router-dom";
+import Logo1C from "../../../images/logo1C.svg";
 
 const Navigation = () => {
     return (
         <nav className={Styles.navigation} >
             <a className={Styles.logo} >VA</a>
             <ul className={Styles.listLinks} >
-                <a className={Styles.link} >Программы 1С</a>
+                <a className={Styles.link} >Программы1C</a>
                 <a className={Styles.link} >Услуги</a>
                 <a className={Styles.link} >Портфолио</a>
                 <a className={Styles.link} >Контакты</a>
             </ul>
             <a className={Styles.partner} >
-                <img className={Styles.partner__logo} alt="логотип 1С" />
+                <img className={Styles.partner__logo} alt="логотип 1C" src={Logo1C} />
                 <p className={Styles.partner__name} >Официальный<br />Партнер</p>
             </a>
         </nav>
