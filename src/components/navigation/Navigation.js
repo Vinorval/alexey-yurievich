d15220013c1059e1858319c3ba680e59c0e2dc44
Vinor <vinorval@yandex.ru>
2024-09-03 11:53:27 +0300

    add the third version on the navigation layout

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -2,16 +2,29 @@ import React from "react";
 import Styles from "./Navigation.module.css";
 import Logo1C from "../../images/logo1C.svg";
 import Arrow from "../../images/arrowDownWhite.svg";
+import { arrProducts, arrServices } from "../../utils/typesProducts";
 
 const Navigation = () => {
     return (
         <nav className={Styles.navigation} >
             <a className={Styles.logo} >VA</a>
             <ul className={Styles.listLinks} >
-                <a className={Styles.link} >Программы1C<img alt="стрелка вниз" className={Styles.link__arrow} src={Arrow} /></a>
-                <a className={Styles.link} >Услуги<img alt="стрелка вниз" className={Styles.link__arrow} src={Arrow} /></a>
-                <a className={Styles.link} >Портфолио</a>
-                <a className={Styles.link} >Контакты</a>
+                <li className={Styles.link} >
+                    <p className={Styles.margin_none} >Программы1C</p>
+                    <img alt="стрелка вниз" className={Styles.link__arrow} src={Arrow} />
+                    <ul className={Styles.link__namesList} >{
+                        arrProducts.map((name, index) => <li key={index} className={Styles.product} >{name}</li>)
+                    }</ul>
+                </li>
+                <li className={Styles.link} >
+                    <p className={Styles.margin_none} >Услуги</p>
+                    <img alt="стрелка вниз" className={Styles.link__arrow} src={Arrow} />
+                    <ul className={Styles.link__namesList} >{
+                        arrServices.map((name, index) => <li key={index} className={Styles.product} >{name}</li>)
+                    }</ul>
+                </li>
+                <li className={Styles.link} ><p className={Styles.margin_none} >Портфолио</p></li>
+                <li className={Styles.link} ><p className={Styles.margin_none} >Контакты</p></li>
             </ul>
             <a className={Styles.partner} >
                 <img className={Styles.partner__logo} alt="логотип 1C" src={Logo1C} />
