a38b0b7c2a8bbdfa40da5dfd4aa6138c75b51b92
Vinor <vinorval@yandex.ru>
2024-09-03 17:06:41 +0300

    add arr of service names

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Styles from "./Navigation.module.css";
 import Logo1C from "../../images/logo1C.svg";
+import Arrow from "../../images/arrowDownWhite.svg";
 
 const Navigation = () => {
     return (
         <nav className={Styles.navigation} >
             <a className={Styles.logo} >VA</a>
             <ul className={Styles.listLinks} >
-                <a className={Styles.link} >Программы1C</a>
-                <a className={Styles.link} >Услуги</a>
+                <a className={Styles.link} >Программы1C<img alt="стрелка вниз" className={Styles.link__arrow} src={Arrow} /></a>
+                <a className={Styles.link} >Услуги<img alt="стрелка вниз" className={Styles.link__arrow} src={Arrow} /></a>
                 <a className={Styles.link} >Портфолио</a>
                 <a className={Styles.link} >Контакты</a>
             </ul>
