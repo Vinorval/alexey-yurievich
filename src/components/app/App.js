afc44eff501931f4219360bff1cd39930b673fe2
Vinor <vinorval@yandex.ru>
2024-09-05 12:17:29 +0300

    add layout services-block

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import AppHeader from '../appHeader/AppHeader';
+import Services from '../services/Services';
 
 function App() {
   return (
     <div className="App">
       <AppHeader />
+      <Services />
+      <footer className="Footer" />
     </div>
   );
 }
