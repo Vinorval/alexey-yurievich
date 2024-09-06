9f56597cbe6cb7b04001b09105af7470441494c6
Vinor <vinorval@yandex.ru>
2024-09-06 13:12:55 +0300

    add styles for services-block

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -34,8 +34,8 @@ const AppHeader = () => {
             </div>
             <div className={Styles.main}>
                 <div className={Styles.main__textWrapper}>
-                    <h1 className={Styles.main__title}>1С Программист</h1>
-                    <h2>Более 20-ти лет работы в области разработки<br /> и поддержки программного обеспечения на базе 1С</h2>      
+                    <h1 className={Styles.title}>1С Программист</h1>
+                    <h2 className={Styles.subtitle} >Более 20-ти лет работы в области разработки<br /> и поддержки программного обеспечения на базе 1С</h2>      
                     <svg xmlns="http://www.w3.org/2000/svg" className={Styles.dottedCircle} width="352" height="352" overflow="visible">
                         <circle cx="176" cy="176" r="174" fill="none" stroke="#fff" strokeWidth="2" strokeMiterlimit="10" strokeDasharray="12.921,11.9271"/>
                     </svg>
