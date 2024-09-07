43f6c8bba0448122998198e336cf3e2d6f8a2435
Vinor <vinorval@yandex.ru>
2024-09-07 14:19:34 +0300

    add styles for form

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -11,11 +11,11 @@ const Form = () => {
             </div>
             <form className={Styles.form}>
                 <fieldset className={Styles.inputs}>
-                    <input className={Styles.input} id="name" placeholder="Ваше имя"></input>
+                    <input className={`${Styles.input} ${Styles.input_width_long}`} id="name" placeholder="Ваше имя"></input>
                     <input className={Styles.input} id="phone" placeholder="Номер телефона"></input>
                     <input className={Styles.input} id="email" placeholder="Email"></input>
-                    <input className={Styles.input} id="theme" placeholder="Тема сообщения"></input>
-                    <textarea className={Styles.input} id="text" placeholder="Сообщение"></textarea>
+                    <input className={`${Styles.input} ${Styles.input_width_long}`} id="theme" placeholder="Тема сообщения"></input>
+                    <textarea className={`${Styles.input} ${Styles.input_type_area} ${Styles.input_width_long}`} id="text" placeholder="Сообщение"></textarea>
                 </fieldset>
                 <button className={Styles.button} type="submit" >Связаться</button>
             </form>
