bd4b2436f70445b4d5f84301c966a12a6f6c3852
Vinor <vinorval@yandex.ru>
2024-09-20 10:00:34 +0300

    change block service

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Styles from "./Services.module.css";
 
+import services from "../../utils/services";
+
 import Product from "../../images/monitor.svg";
 import Gear from "../../images/gears.svg";
 import Integration from "../../images/illustration-jigsaw-icon.svg";
@@ -8,7 +10,7 @@ import Integration from "../../images/illustration-jigsaw-icon.svg";
 const Services = () => {
     return (
         <section className={Styles.services} >
-            <h2 className={Styles.services__title}>Услуги 1С программиста</h2>
+            {/*<h2 className={Styles.services__title}>Услуги 1С программиста</h2>
             <ul className={Styles.list} >
                 <li className={Styles.service} >
                     <img alt="экран монитора" src={Product} className={Styles.service__image} />
@@ -25,6 +27,21 @@ const Services = () => {
                     <h3 className={Styles.service__type} >Интеграция 1С</h3>
                     <p className={Styles.service__text} >Интеграция 1С позволит автоматизировать бизнес-процессы, повысить точность получаемой информации и минимизировать ошибки</p>
                 </li>
+            </ul>*/}
+            <div className={Styles.blockTitle} >
+                <h2 className={Styles.title} >Услуги 1С программиста</h2>
+                <p className={Styles.blockTitle__text} >Профессиональная помощь в автоматизации бизнес-процессов с использованием программ на платформе 1С:Предприятие.</p>
+                <p className={Styles.blockTitle__text} >Занимаюсь разработкой, настройкой и внедрением программного обеспечения, адаптированного под потребности предприятия.</p>
+            </div>
+            <ul className={Styles.list} >
+                { services.map(( item, index ) => {
+                    return (
+                        <li className={Styles.service} key={index} >
+                            <h3 className={Styles.service__name} >{item.name}</h3>
+                            <button className={Styles.service__button} >Подробнее</button>
+                        </li>
+                    )
+                }) }
             </ul>
         </section>
     )
