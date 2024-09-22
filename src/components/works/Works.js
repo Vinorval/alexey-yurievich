1e9975f4d0dfefe98c243dc7898481f5d325eb81
Vinor <vinorval@yandex.ru>
2024-09-22 13:00:56 +0300

    highlight an active company

--- src/components/works/Works.js
+++ src/components/works/Works.js
@@ -2,15 +2,19 @@ import React from "react";
 import Styles from "./Works.module.css";
 
 const Works = () => {
+    const [ id, setId ] = React.useState(1);
+
+    //const clickOnCompanyName = (id) => setId(id);
+
     return (
         <section className={Styles.works} >
             <h2 className={Styles.works__title} >Примеры работ</h2>
             <ul className={Styles.company} >
-                <li className={Styles.company__name} >ООО “Название компании”</li>
-                <li className={Styles.company__name} >ООО “Название компании”</li>
-                <li className={Styles.company__name} >ООО “Название компании”</li>
-                <li className={Styles.company__name} >ООО “Название компании”</li>
-                <li className={Styles.company__name} >ООО “Название компании”</li>
+                <li onClick={() => setId(1)} className={`${Styles.company__name} ${ (id === 1) && Styles.company__name_active }`} >ООО “Название компании”</li>
+                <li onClick={() => setId(2)} className={`${Styles.company__name} ${ (id === 2) && Styles.company__name_active }`} >ООО “Название компании”</li>
+                <li onClick={() => setId(3)} className={`${Styles.company__name} ${ (id === 3) && Styles.company__name_active }`} >ООО “Название компании”</li>
+                <li onClick={() => setId(4)} className={`${Styles.company__name} ${ (id === 4) && Styles.company__name_active }`} >ООО “Название компании”</li>
+                <li onClick={() => setId(5)} className={`${Styles.company__name} ${ (id === 5) && Styles.company__name_active }`} >ООО “Название компании”</li>
             </ul>
             <div className={Styles.experience} >
                 <div className={Styles.work}>
